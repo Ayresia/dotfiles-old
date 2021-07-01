71169119a2317cacdb493924708ea1293a60786e
Ayresia <nalizyoncatania0@gmail.com>
2021-07-01 20:41:22 +0000

    Removed statusline and install packer if not already.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,8 +1,17 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+-- Automatically instally packer if it's not installed already.
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
+  execute 'packadd packer.nvim'
+end
+
 return require('packer').startup(
 	function(use)
 		use 'wbthomason/packer.nvim'
 		use 'owozsh/Amora'
-		-- use 'itchyny/lightline.vim'
 		use 'neovim/nvim-lspconfig'
 		use 'hrsh7th/nvim-compe'
 		use 'nvim-lua/popup.nvim'
