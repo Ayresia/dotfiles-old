99ac72d276a19f2d7238be804f2c5795edebd444
Ayresia <nalizyoncatania0@gmail.com>
2022-02-04 13:26:46 +0100

    Fix packer.nvim auto-install

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,14 +1,14 @@
-local execute = vim.api.nvim_command
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
--- Automatically instally packer if it's not installed already.
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
-  execute 'packadd packer.nvim'
+  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+  vim.cmd('packadd packer.nvim')
 end
 
-return require('packer').startup(
+local packer = require('packer')
+
+return packer.startup(
     function(use)
         use 'wbthomason/packer.nvim'
         use 'neovim/nvim-lspconfig'
@@ -23,5 +23,9 @@ return require('packer').startup(
         use 'nvim-telescope/telescope.nvim'
         use 'j-hui/fidget.nvim'
         use 'andweeb/presence.nvim'
+
+        if packer_bootstrap then
+            packer.sync()
+        end
     end
 )
